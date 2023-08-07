c177435a97d6122be89ec52cb71911643711f89e
Ian Zelbo <113198991+izelbo129@users.noreply.github.com>
2023-08-07 18:56:15 -0400

    Add files via upload

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ian Zelbo on 7/19/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
